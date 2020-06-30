d6d0444619ca655bddd89c8d2bae0fefa2c89e96
Ariel Ayala <arielayalamail@gmail.com>
2020-06-30 18:17:56 -0400

    Agregado de estilos y creación de modal

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,12 +10,14 @@ import { PlayerService } from './services/player.service';
 import { TeamService } from './services/team.service';
 import { TeamTableComponent } from './team-table/team-table.component';
 import { PlayerTableComponent } from './player-table/player-table.component';
+import { PlayerDialogComponent } from './player-dialog/player-dialog.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TeamTableComponent,
-    PlayerTableComponent
+    PlayerTableComponent,
+    PlayerDialogComponent
   ],
   imports: [
     BrowserModule,
